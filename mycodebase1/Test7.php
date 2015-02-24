311ef8a5d89337c5ff5d6db682659aabd94e66f1
Joe Favara <JFavara08@gmail.com>
2015-02-24 12:03:12 -0500

    First Commit of project Second commit Restore flle third time a charm adding Test7

---
+++ mycodebase1/Test7.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josephfavara
+ * Date: 2/24/15
+ * Time: 11:53 AM
+ * just playing around with git and github
+ *
+ */
+
+$host = 'localhost';
+$host1 = "127.0.0.1";
+$port = "8889";
+$user = "root";
+$password = "root";
+$database = 'php-mvc';
+
+$test2 = null;
+
